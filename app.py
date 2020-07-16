40472f1549975333a97cdfb8cbaa0072c700b456
Manoj Kumar <gcssm1999@gmail.com>
2020-07-16 22:14:09 +0530

    second commit

--- app.py
+++ app.py
@@ -17,10 +17,43 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/result")
+@app.route("/result", methods = ['POST', 'GET'])
 def result():
-    lst=[70,1,4,130,322,0,2,109,0,2.4,2,3,3]
-    result=prediction(lst)
-    return render_template("result.html",result=result)
+    if request.method=='POST':
+        age=request.form["age"]
+        serum_cholestoral=request.form["serum_cholestoral"]
+        chest=request.form["chest"]	
+        resting_blood_pressure=request.form["resting_blood_pressure"]
+        maximum_heart_rate_achieved=request.form["maximum_heart_rate_achieved"]
+        oldpeak=request.form["oldpeak"]
+        thal=request.form["thal"]
+        sex=request.form["age"]
+        fasting_blood_sugar=request.form['fasting_blood_sugar']	
+        resting_electrocardiographic_results=request.form['resting_electrocardiographic_results']		
+        exercise_induced_angina=request.form['exercise_induced_angina']
+        slope=request.form['slope']
+        number_of_major_vessels=request.form['number_of_major_vessels']
+        lst=list()
+        lst.append((age))
+        lst.append((sex))
+        lst.append((chest))
+        lst.append((resting_blood_pressure))
+        lst.append((serum_cholestoral))
+        lst.append((fasting_blood_sugar))
+        lst.append((resting_electrocardiographic_results))
+        lst.append((maximum_heart_rate_achieved))
+        lst.append((exercise_induced_angina))
+        lst.append((oldpeak))
+        lst.append((slope))
+        lst.append((number_of_major_vessels))
+        lst.append((thal))
+        result=prediction(lst)
+        return render_template("result.html",result=result,lst=lst)
+
+
+
+    else:
+        return render_template("index.html")
+
 if __name__=='__main__':
     app.run( debug=True)
--- test.py
+++ test.py
@@ -13,20 +13,19 @@ from sklearn.linear_model import LogisticRegression
 
 
 def prediction(new_data):
-    new_data=np.array(new_data)
+    df=pd.read_csv('heart-data.csv')
+    df.rename(columns={"class":"target"},inplace=True)
+    df['target'].replace(['absent','present'],[0,1],inplace=True)
+    df=pd.get_dummies(df)
+    x=df.drop('target', axis=1)
+    y=df['target']
+    column=x.columns
+    train_x,valid_x,train_y,valid_y=train_test_split(x,y,test_size=0.3,random_state=35)
+    logr=LogisticRegression()
+    logr.fit(train_x,train_y)
+    new_data=np.array(new_data,dtype='int64')
     new_data=new_data.reshape(1,13)
-    result=logr.predict(new_data)
+    xnew_data=pd.DataFrame(new_data)
+    result=logr.predict(xnew_data)
     return result[0]
-df=pd.read_csv('heart-data.csv')
-df.rename(columns={"class":"target"},inplace=True)
-df['target'].replace(['absent','present'],[0,1],inplace=True)
-df=pd.get_dummies(df)
-x=df.drop('target', axis=1)
-y=df['target']
-train_x,valid_x,train_y,valid_y=train_test_split(x,y,test_size=0.3,random_state=35)
-logr=LogisticRegression()
-logr.fit(train_x,train_y)
-pred_test=logr.predict(valid_x)
-new_data=[70,1,4,130,322,0,2,109,0,2.4,2,3,3]
-print(prediction(new_data))
 
